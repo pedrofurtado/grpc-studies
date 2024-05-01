3cadda623f25ad39719ddca276e9e0876cda8ff7
Pedro Furtado <pedro.furtado@locaweb.com.br>
2024-05-01 13:56:19 -0300

    Initial gRPC implementation for NodeJS

--- apps-nodejs/client/app.js
+++ apps-nodejs/client/app.js
@@ -7,9 +7,21 @@ const fetch = require('node-fetch')
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.json({
-    app: 'GRPC studies - Apps NodeJS - Client'
+  const gRPC = require('grpc')
+  const ProductService = gRPC.load('/proto/products.proto').ProductService
+
+  const client = new ProductService('server_nodejs:5000', gRPC.credentials.createInsecure())
+
+  client.list({}, (error, products) => {
+    res.json({
+      error: error,
+      data_from_grpc: products
+    })
   })
+
+  // res.json({
+  //   app: 'GRPC studies - Apps NodeJS - Client'
+  // })
 })
 
 app.listen(3000, () => {
--- apps-nodejs/server/app.js
+++ apps-nodejs/server/app.js
@@ -12,6 +12,25 @@ app.get('/', (req, res) => {
   })
 })
 
+const gRPC = require('grpc')
+const productsProto = gRPC.load('/proto/products.proto')
+const gRPCServer = new gRPC.Server()
+
+gRPCServer.bind('0.0.0.0:5000', gRPC.ServerCredentials.createInsecure())
+
+gRPCServer.addService(productsProto.ProductService.service, {
+  list: (_, callback) => {
+    const products = [
+      { id: '10', name: 'Product 10', description: 'Description 10' },
+      { id: '20', name: 'Product 20', description: 'Description 20' }
+    ]
+    callback(null, products)
+  },
+})
+
 app.listen(3000, () => {
-  console.log('GRPC studies - Apps NodeJS - Server - App started')
+  console.log('GRPC studies - Apps NodeJS - Server - Web App started')
+
+  console.log('GRPC studies - Apps NodeJS - Server - GRPC App started')
+  gRPCServer.start()
 })
