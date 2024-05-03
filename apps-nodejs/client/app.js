48a578eca6f37a6b3cacf0c70de83e574882b478
Pedro Furtado <pedro.furtado@locaweb.com.br>
2024-05-03 20:11:05 -0300

    NodeJS apps for gRPC

--- apps-nodejs/client/app.js
+++ apps-nodejs/client/app.js
@@ -1,28 +1,76 @@
 const express = require('express')
 const app = express()
-const os = require('os')
 const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
+const gRPC = require('grpc')
+const productProto = gRPC.load('/proto/products.proto')
+const productProtoClient = new productProto.ProductService(process.env.GRPC_SERVER, gRPC.credentials.createInsecure())
 
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  const gRPC = require('grpc')
-  const productProto = gRPC.load('/proto/products.proto')
+  res.json({
+    app: 'GRPC studies - Apps NodeJS - Client - Homepage'
+  })
+})
 
-  const client = new productProto.ProductService(process.env.GRPC_SERVER, gRPC.credentials.createInsecure())
-
-  client.listAll({}, (error, data) => {
+app.get('/api/products', (req, res) => {
+  productProtoClient.listAll({}, (error, data) => {
     res.json({
-      service: 'listAll',
+      app: 'GRPC studies - Apps NodeJS - Client - GET /api/products (GRPC ListAll)',
       error: error,
       data: data
     })
   })
+})
 
-  // res.json({
-  //   app: 'GRPC studies - Apps NodeJS - Client'
-  // })
+app.get('/api/products/:id', (req, res) => {
+  const getProductParams = { id: req.params.id }
+
+  productProtoClient.get(getProductParams, (error, data) => {
+    res.json({
+      app: 'GRPC studies - Apps NodeJS - Client - GET /api/products/:id (GRPC Get)',
+      error: error,
+      data: data
+    })
+  })
+})
+
+app.post('/api/products', (req, res) => {
+  productProtoClient.insert(req.body, (error, data) => {
+    res.json({
+      app: 'GRPC studies - Apps NodeJS - Client - POST /api/products (GRPC Insert)',
+      error: error,
+      data: data
+    })
+  })
+})
+
+app.put('/api/products/:id', (req, res) => {
+  const updateProductParams = {
+    id: req.params.id,
+    name: req.body.name,
+    description: req.body.description
+  }
+
+  productProtoClient.update(updateProductParams, (error, data) => {
+    res.json({
+      app: 'GRPC studies - Apps NodeJS - Client - PUT /api/products/:id (GRPC Update)',
+      error: error,
+      data: data
+    })
+  })
+})
+
+app.delete('/api/products/:id', (req, res) => {
+  const deleteProductParams = { id: req.params.id }
+
+  productProtoClient.delete(deleteProductParams, (error, data) => {
+    res.json({
+      app: 'GRPC studies - Apps NodeJS - Client - DELETE /api/products/:id (GRPC Delete)',
+      error: error,
+      data: data
+    })
+  })
 })
 
 app.listen(3000, () => {
--- apps-nodejs/server/app.js
+++ apps-nodejs/server/app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const gRPC = require('grpc')
+const productsProto = gRPC.load('/proto/products.proto')
+const gRPCServer = new gRPC.Server()
+gRPCServer.bind('0.0.0.0:5000', gRPC.ServerCredentials.createInsecure())
 
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.json({
-    app: 'GRPC studies - Apps NodeJS - Server'
+    app: 'GRPC studies - Apps NodeJS - Server - Homepage'
   })
 })
 
@@ -26,12 +30,6 @@ function generateProductsForFakeDatabaseInMemory(numberOfProducts) {
 
 const DBInMemory = generateProductsForFakeDatabaseInMemory(4)
 
-const gRPC = require('grpc')
-const productsProto = gRPC.load('/proto/products.proto')
-const gRPCServer = new gRPC.Server()
-
-gRPCServer.bind('0.0.0.0:5000', gRPC.ServerCredentials.createInsecure())
-
 gRPCServer.addService(productsProto.ProductService.service, {
   listAll: (_, callback) => {
     callback(null, {
@@ -43,50 +41,56 @@ gRPCServer.addService(productsProto.ProductService.service, {
 
   get: (call, callback) => {
     const productId = call.request.id
-    let product = DBInMemory.find((n) => n.id == productId)
+    let product = DBInMemory.find((p) => p.id == productId)
 
     if (product) {
       callback(null, product)
     }
     else {
       callback({
-        code: grpc.status.NOT_FOUND,
-        details: `Product with id=${productId} not found`
+        code: gRPC.status.NOT_FOUND, // All available gRPC status: https://grpc.github.io/grpc/core/md_doc_statuscodes.html
+        details: `Product with id=${productId} not found for get`
       })
     }
   },
 
   insert: (call, callback) => {
-    let note = call.request
-    note.id = uuidv1()
-    notes.push(note)
-    callback(null, note)
+    const product = call.request
+    product.id = Math.max(...DBInMemory.map(p => p.id)) + 1
+    DBInMemory.push(product)
+    callback(null, product)
   },
 
   update: (call, callback) => {
-    let existingNote = notes.find((n) => n.id == call.request.id)
-    if (existingNote) {
-        existingNote.title = call.request.title
-        existingNote.content = call.request.content
-        callback(null, existingNote)
-    } else {
-        callback({
-            code: grpc.status.NOT_FOUND,
-            details: "Not found"
-        })
+    const productId = call.request.id
+    const existingProduct = DBInMemory.find((p) => p.id == productId)
+
+    if (existingProduct) {
+      existingProduct.name = call.request.name
+      existingProduct.description = call.request.description
+      callback(null, existingProduct)
+    }
+    else {
+      callback({
+        code: gRPC.status.NOT_FOUND,
+        details: `Product with id=${productId} not found for update`
+      })
     }
   },
 
   delete: (call, callback) => {
-    let existingNoteIndex = notes.findIndex((n) => n.id == call.request.id)
-    if (existingNoteIndex != -1) {
-        notes.splice(existingNoteIndex, 1)
-        callback(null, {})
-    } else {
-        callback({
-            code: grpc.status.NOT_FOUND,
-            details: "Not found"
-        })
+    const productId = call.request.id
+    const existingProductIndex = DBInMemory.findIndex((p) => p.id == productId)
+
+    if (existingProductIndex != -1) {
+      DBInMemory.splice(existingProductIndex, 1)
+      callback(null, {})
+    }
+    else {
+      callback({
+        code: gRPC.status.NOT_FOUND,
+        details: `Product with id=${productId} not found for delete`
+      })
     }
   }
 })
